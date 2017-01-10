b6ae44dcf7b9af48d2bde718b9c3dbb3d0697f85
Chris Esplin <chris@quiver.is>
2017-01-10 12:39:03 -0700

    Console logs

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ var admin = require("firebase-admin");
 var env = require('./services/environment');
 var _ = require('lodash');
 
+console.log('environment', env);
+
 admin.initializeApp({
   credential: admin.credential.cert(env.firebaseConfig.serviceAccount),
   databaseURL: env.firebaseConfig.databaseURL
 });
 
 var ref = admin.database().ref();
+
+console.log(ref.toString());
 
 var searchService = require('./services/search')(ref);
 var algoliaService = require('./services/algolia')(ref);
@@ -16,20 +20,24 @@ var algoliaService = require('./services/algolia')(ref);
 //   console.log('numChildren', snap.numChildren());
 // });
 
-
+console.log(1);
 algoliaService.start()
   .then(function () {
+    console.log(2);
     return searchService.listenToPosts();
   });
 
 ref.child('Queue/rebuild').on('child_changed', function() {
+  console.log(3);
   algoliaService.stop();
 
   return searchService.rebuild()
     .then(function() {
+      console.log(4);
       return algoliaService.build();
     })
     .then(function() {
+      console.log(5);
       return algoliaService.start();
     });
 });
