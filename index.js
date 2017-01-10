d5946020b695d3d81632dbae954f4ca625ef550a
Chris Esplin <chris@quiver.is>
2017-01-10 13:03:32 -0700

    Removing comments

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var admin = require("firebase-admin");
 var env = require('./services/environment');
 var _ = require('lodash');
 
-console.log('environment', env);
+// console.log('environment', env);
 
 admin.initializeApp({
   credential: admin.credential.cert(env.firebaseConfig.serviceAccount),
@@ -17,24 +17,19 @@ var algoliaService = require('./services/algolia')(ref);
 //   console.log('numChildren', snap.numChildren());
 // });
 
-console.log(1);
 algoliaService.start()
   .then(function () {
-    console.log(2);
     return searchService.listenToPosts();
   });
 
 ref.child('Queue/rebuild').on('child_changed', function() {
-  console.log(3);
   algoliaService.stop();
 
   return searchService.rebuild()
     .then(function() {
-      console.log(4);
       return algoliaService.build();
     })
     .then(function() {
-      console.log(5);
       return algoliaService.start();
     });
 });
