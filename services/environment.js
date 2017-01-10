6dbbb811b04dfc7dd31d2d29cbb8a45424195ec3
Chris Esplin <chris@quiver.is>
2017-01-10 11:59:26 -0700

    Fixing the service account bug.

--- services/environment.js
+++ services/environment.js
@@ -4,7 +4,11 @@ try {
 } catch (e) {
   env = {
     "firebaseConfig": {
-      "serviceAccount": process.env.FIREBASE_SERVICE_ACCOUNT,
+      "serviceAccount": {
+        "projectId": process.env.FIREBASE_PROJECT_ID,
+        "clientEmail": process.env.FIREBASE_CLIENT_EMAIL,
+        "privateKey": process.env.FIREBASE_PRIVATE_KEY,
+      },
       "databaseURL": process.env.FIREBASE_DATABASE_URL
     },
     "algolia": {
