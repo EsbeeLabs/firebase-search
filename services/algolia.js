efdd1745ad1fda9df7e3940eec2d58eb4ef89dc0
Chris Esplin <chris@quiver.is>
2017-01-10 13:38:54 -0700

    Node engines

--- services/algolia.js
+++ services/algolia.js
@@ -2,14 +2,10 @@ var FirebaseSearch = require('quiver-firebase-search');
 var env = require('./environment');
 
 module.exports = function (ref) {
-  console.log('inside algolia.js', env);
   var commentsSearchRef = ref.child('Search/Comments');
   var search = new FirebaseSearch(commentsSearchRef, {
     algolia: env.algolia
   }, 'Comments');
-
-  console.log(commentsSearchRef.toString());
-  console.log(search.algolia.firebase.start);
 
   return {
     build: search.algolia.firebase.build.bind(search.algolia.firebase),
