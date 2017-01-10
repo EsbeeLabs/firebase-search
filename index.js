2a4263ce17412e194e8dabe58efee601a364c1c0
Chris Esplin <chris@quiver.is>
2017-01-10 13:16:39 -0700

    Adding catch

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var admin = require("firebase-admin");
 var env = require('./services/environment');
 var _ = require('lodash');
 
-// console.log('environment', env);
 
 admin.initializeApp({
   credential: admin.credential.cert(env.firebaseConfig.serviceAccount),
@@ -15,11 +14,16 @@ var algoliaService = require('./services/algolia')(ref);
 
 // ref.child('Search/Comments').once('value', function(snap) {
 //   console.log('numChildren', snap.numChildren());
-// });
+// }
 
+console.log(1);
 algoliaService.start()
   .then(function () {
+    console.log(2);
     return searchService.listenToPosts();
+  })
+  .catch(function(err) {
+    console.log('err', err);
   });
 
 ref.child('Queue/rebuild').on('child_changed', function() {
